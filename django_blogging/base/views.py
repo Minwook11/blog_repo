5ae81323093fcff4cb76873ae8e46c03c9d6168a
Minwook11 <alsdnr4874@gmail.com>
2021-09-17 16:15:56 +0900

    API 작동 성공 로그 기록 코드 추가

--- django_blogging/base/views.py
+++ django_blogging/base/views.py
@@ -1,9 +1,13 @@
 import json
+import logging
+import datetime
 
 from django.views import View
 from django.http import JsonResponse
 
 from .models import *
+
+logger = logging.getLogger('django')
 
 class CaseView(View):
     def post(self, request):
@@ -13,5 +17,7 @@ class CaseView(View):
         code = data['code']
 
         test, flag = Case.objects.get_or_create(name = name, code = code)
+
+        logger.info("{} {} {} 200".format(request.method, request.path, request.headers['User-Agent']))
 
         return JsonResponse({'Message' : 'Check', 'Result' : test.id}, status = 200)

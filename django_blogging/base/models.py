ab5ffe64d620209b6d281dcd0ff44a649e6aed56
Minwook11 <alsdnr4874@gmail.com>
2021-10-05 21:38:28 +0900

    다양한 ORM 사용을 위한 테스트 모델 수정 진행

--- django_blogging/base/models.py
+++ django_blogging/base/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Case(models.Model):
     name = models.CharField(max_length=200)
     code = models.IntegerField()
+
+class Level(models.Model):
+    number = models.IntegerField()
+    level = models.TextField()
+
+class Complex(models.Model):
+    case = models.ForeignKey('Case', on_delete = models.CASCADE)
+    level = models.ForeignKey('Level', on_delete = models.CASCADE)
+    key_1 = models.IntegerField()
+    key_2 = models.IntegerField()

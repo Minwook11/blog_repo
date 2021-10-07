55535eb30c633d1d79509dd7daa359b718405720
Minwook11 <alsdnr4874@gmail.com>
2021-10-07 20:28:48 +0900

    admin.py 에서 참조 관계 테이블의 속성(Attribute) 사용하는 방법 - 해당 속성을 불러오는 메소드를 만들어야 한다.

--- django_blogging/base/admin.py
+++ django_blogging/base/admin.py
@@ -8,7 +8,13 @@ class Registered_Level(admin.ModelAdmin):
     list_display = ('id', 'number', 'level')
 
 class Registered_Complex(admin.ModelAdmin):
-    list_display = ('id', 'case', 'level', 'key_1', 'key_2')
+    list_display = ('id', 'get_Case', 'get_Level', 'key_1', 'key_2')
+
+    def get_Case(self, obj):
+        return obj.case.name
+
+    def get_Level(self, obj):
+        return obj.level.level
 
 class Registered_Product(admin.ModelAdmin):
     list_display = ('id', 'name')
@@ -17,7 +23,13 @@ class Registered_Size(admin.ModelAdmin):
     list_display = ('id', 'name', 'weight')
 
 class Registered_SpecificProduct(admin.ModelAdmin):
-    list_display = ('id', 'product', 'size')
+    list_display = ('id', 'get_Product', 'get_Size')
+
+    def get_Product(self, obj):
+        return obj.product.name
+
+    def get_Size(self, obj):
+        return obj.size.name
 
 admin.site.register(Case, Registered_Case)
 admin.site.register(Level, Registered_Level)

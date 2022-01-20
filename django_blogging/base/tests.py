b0dfd5a465af171197153e1ac744bcc88f00d7a9
Minwook11 <alsdnr4874@gmail.com>
2022-01-20 19:22:55 +0900

    UnitTest - Failed case - KeyError 발생하는 경우에 해당되는 테스트 케이스 적용해봄 - 다른 발생가능 예외에 대해서 생각해보고 적용해보려고 한다. * 지금 탭이 이상하게 작동하고 있다. Tab키 사용하면 TabError 발생하고 있음

--- django_blogging/base/tests.py
+++ django_blogging/base/tests.py
@@ -10,13 +10,22 @@ class UnitTest(TestCase):
         'name' : 'test_case',
         'code' : 0
     }
+    wrong_name_key = {
+        'neme' : 'test_case',
+        'code' : 0
+    }
 
-    def test_CaseView_POST(self):
+    def test_CaseView_POST_SUCCESS(self):
         response = client.post('/base/case', json.dumps(self.test_case_data), content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
 
-    def test_caseView_GET(self):
+    def test_caseView_GET_SUCCESS(self):
         client.post('/base/case', json.dumps(self.test_case_data), content_type = 'application/json/json')
 
         response = client.get('/base/case', content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
+
+    def test_CaseView_POST_WRONG_DATA_KEY_NAME(self):
+
+        response = client.post('/base/case', json.dumps(self.wrong_name_key), content_type = 'application/json')
+        self.assertEqual(response.status_code, 400)
